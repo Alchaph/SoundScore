3c0bd8cc8b98be0192ef11245708500eb705f55b
e550036 <timo.balsiger@sbb.ch>
2024-05-03 14:53:18 +0200

    feat: Componets inizialized

---
+++ client/src/app/Interceptors/TokenInterceptor.ts
@@ -0,0 +1,13 @@
+import {HttpInterceptorFn} from "@angular/common/http";
+
+export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const methods = ['GET', 'POST', 'PUT', 'DELETE'];
+  if (methods.includes(req.method)) {
+    const token = sessionStorage.getItem('token');
+    if (token) {
+      req.headers.set('Authorization', `Bearer ${token}`);
+    }
+    return next(req);
+  }
+  return next(req);
+}
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,9 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {TranslateService} from "@ngx-translate/core";
-import {provideHttpClient, withFetch} from "@angular/common/http";
+import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 import {provideAnimations} from "@angular/platform-browser/animations";
+import {tokenInterceptor} from "./Interceptors/TokenInterceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideAnimations()]
+  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideAnimations(), provideHttpClient(withInterceptors([tokenInterceptor]))],
 };
