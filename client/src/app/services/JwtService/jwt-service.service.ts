8b591361d236ba8d3a0d3a1386a1ca116af368bd
e550036 <timo.balsiger@sbb.ch>
2024-05-03 08:27:23 +0200

    Frontend jwtService implementiert

--- client/src/app/services/JwtService/jwt-service.service.ts
+++ client/src/app/services/JwtService/jwt-service.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../../models/User";
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  public login(email: string, password: string){
+    return this.http.post('http://localhost:8080/api/auth/login', {
+      email: email,
+      password: password
+    });
+  }
+
+  public register(email: string, password: string, roleID: number){
+    return this.http.post('http://localhost:8080/api/auth/signup', {
+      email: email,
+      password: password,
+      roleID: roleID
+    });
+  }
+
+  public getUsers(){
+    return this.http.get<User[]>('http://localhost:8080/api/users/', {
+      headers: {
+        Authorization: 'Bearer ' + sessionStorage.getItem('token')
+      }
+    });
+  }
+
+  public getMe(){
+    return this.http.get<User>('http://localhost:8080/api/users/me', {
+      headers: {
+        Authorization: 'Bearer ' + sessionStorage.getItem('token')
+      }
+    });
+  }
 }
