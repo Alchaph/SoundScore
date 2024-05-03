8e7d43ef0b1eb2aed2fcfb0a69fb33f7fbe8c08a
e550036 <timo.balsiger@sbb.ch>
2024-05-03 13:15:41 +0200

    feat: Componets inizialized

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
