29a740091545f287fac6c8c5f719fb72d4f80cdc
digennaro <digennaro@puzzle.ch>
2024-05-24 09:24:14 +0200

    Feat: Artist Profile

---
+++ client/src/app/components/add-artist/add-artist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-artist',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-artist.component.html',
+  styleUrl: './add-artist.component.scss'
+})
+export class AddArtistComponent {
+
+}
