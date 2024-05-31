1d5fc6f4bc9dcd9535ae8395ee71800ccb18bf97
digennaro <digennaro@puzzle.ch>
2024-05-31 13:29:18 +0200

    Fix: artist-profile

--- client/src/app/services/GifService/gif.service.ts
+++ client/src/app/services/GifService/gif.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
@@ -7,12 +7,16 @@ import {Observable} from "rxjs";
 })
 export class GifService {
   tenorKey: string = " AIzaSyB2mAvrM-f9yduZfFsVgysJnX5ATx1zon0"
+
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+  }
 
-  public  searchGif(searchString : string) :Observable<{results: { id: number, title: string, media_formats: { gif: { url: string } } }[]}>{
-  return   this.http.get<{
+  public searchGif(searchString: string): Observable<{
+    results: { id: number, title: string, media_formats: { gif: { url: string } } }[]
+  }> {
+    return this.http.get<{
       results: { id: number, title: string, media_formats: { gif: { url: string } } }[]
     }>(`https://tenor.googleapis.com/v2/search?q=${searchString}&key=${this.tenorKey}&client_key=my_test_app&limit=8`)
   }
