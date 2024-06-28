262e3f83f4753e86e67e2ca2efb854764d08d529
e550036 <timo.balsiger@sbb.ch>
2024-06-28 11:46:18 +0200

    feat: ohter usersProfiles

--- client/src/app/services/LoaderService/loader.service.ts
+++ client/src/app/services/LoaderService/loader.service.ts
@@ -8,14 +8,17 @@ export class LoaderService {
   public isLoading = new BehaviorSubject(false);
 
   show() {
+    console.log('show');
     this.isLoading.next(true);
   }
 
   hide() {
+    console.log('hide');
     this.isLoading.next(false);
   }
 
   getIsLoading() {
+    console.log(this.isLoading.getValue());
     return this.isLoading.asObservable();
   }
 }
