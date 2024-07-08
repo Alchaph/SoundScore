16340e7b9a948b8ba267a9d0355d6c8e9fd90dee
digennaro <digennaro@puzzle.ch>
2024-07-08 12:50:33 +0200

    Fix: Home component

--- client/src/app/services/HomeService/home.service.ts
+++ client/src/app/services/HomeService/home.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, OnInit} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Post} from "../../models/Post";
 import {Song} from "../../models/Song";
 import {Genre} from "../../models/Genre";
@@ -9,7 +9,7 @@ import {LeaderBoardService} from "../LeaderBoardService/leader-board.service";
 @Injectable({
   providedIn: 'root'
 })
-export class HomeService implements OnInit {
+export class HomeService {
   posts: Post[] = [];
 
   topSongs: Song[] = [];
@@ -17,36 +17,32 @@ export class HomeService implements OnInit {
   topArtists: Artist[] = [];
 
   constructor(private postService: PostService, private leaderBoardService: LeaderBoardService) {
-  }
-
-  ngOnInit() {
     this.postService.getPosts().subscribe((data: Post[]) => {
       this.posts = data.reverse();
     });
     this.leaderBoardService.getLeaderBoardByGenre().subscribe((data: Genre[]) => {
       this.topGenres = data.reverse();
-      // console.log(this.topGenres)
     });
     this.leaderBoardService.getLeaderBoardByArtist().subscribe((data: Artist[]) => {
       this.topArtists = data.reverse();
-      // console.log(this.topArtists)
     });
     this.leaderBoardService.getLeaderBoardBySong().subscribe((data: Song[]) => {
       this.topSongs = data.reverse();
-      // console.log(this.topSongs)
-
     });
   }
 
   getPosts() {
     return this.posts;
   }
+
   getTopSongs() {
     return this.topSongs;
   }
+
   getTopGenres() {
     return this.topGenres;
   }
+
   getTopArtists() {
     return this.topArtists;
   }
