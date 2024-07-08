d406b1eaedcc3c640c5e783e320a58158bf599ec
e550212 <pranprad_marc.sutthaloung@sbb.ch>
2024-07-08 09:51:06 +0200

    fix: wrong url in environments.ts

--- client/src/environments/environments.ts
+++ client/src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  url: 'http://localhost:8080/api/',
+  url: 'http://localhost:8080/api',
   languages: ['de', 'en', 'fr', 'zh', 'pt', 'Zch', 'e', 'es'],
   options: {
     headers: {
