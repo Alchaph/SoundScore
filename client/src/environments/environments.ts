9349374da5a65b1ed443d36ab01d2b49e238f587
e550036 <timo.balsiger@sbb.ch>
2024-07-10 13:21:12 +0200

    fix: environemts fucked up

--- client/src/environments/environments.ts
+++ client/src/environments/environments.ts
@@ -1,19 +1,9 @@
 import {Language} from "../app/enums/language";
 
 export const environment: {
-  url: 'http://localhost:8080/api',
+  url: string,
   languages: (keyof typeof Language)[],
-  options: {
-    headers: {
-      Authorization: string
-    }
-  }
 } = {
   url: 'http://localhost:8080/api',
   languages: ['de', 'en', 'fr', 'zh', 'pt', 'Zch', 'e', 'es'],
-  options: {
-    headers: {
-      Authorization: 'Bearer ' + localStorage.getItem('token')
-    }
-  }
 };
