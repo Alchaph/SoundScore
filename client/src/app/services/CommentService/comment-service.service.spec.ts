561c8af2615c4a4fac35a403acb86a6416f6c2ea
e550036 <timo.balsiger@sbb.ch>
2024-07-12 10:44:33 +0200

    fix: environemts fucked up

--- client/src/app/services/CommentService/comment-service.service.spec.ts
+++ client/src/app/services/CommentService/comment-service.service.spec.ts
@@ -1,16 +1,490 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CommentService } from './comment.service';
+import {environment} from "../../../environments/environments";
+import {Comment} from "../../models/Comment";
+import {Song} from "../../models/Song";
 
-describe('CommentServiceService', () => {
+describe('CommentService', () => {
   let service: CommentService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CommentService]
+    });
     service = TestBed.inject(CommentService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('createComment should post and return the comment', () => {
+    const mockComment: Comment = {
+      id: 1,
+      title: 'Test Title',
+      message: 'This is a test comment message.',
+      post: {
+        id: 1,
+        title: 'Test Post',
+        content: 'This is a test post content',
+        user: {
+          id: 1,
+          username: 'Test User',
+          email: 'test@example.com',
+          password: 'password',
+          artist: {
+            id: 1,
+            name: 'Test Artist',
+            description: 'This is a test artist description.',
+            image: 'test-artist.jpg'
+          },
+        },
+        likes: [],
+        dislikes: [],
+        image: ''
+      },
+      user: {
+        id: 1,
+        username: 'Test User',
+        email: 'test@example.com',
+        password: 'password',
+        artist: {
+          id: 1,
+          name: 'Test Artist',
+          description: 'This is a test artist description.',
+          image: 'test-artist.jpg'
+        },
+      },
+    };
+    service.createComment(mockComment).subscribe(comment => {
+      expect(comment).toEqual(mockComment);
+    });
+
+    const req = httpMock.expectOne(`${environment.url}/comments`);
+    expect(req.request.method).toBe('POST');
+    req.flush(mockComment);
+  });
+
+  it('createComment should throw an error with invalid http Options', () => {
+    const mockComment: Comment = {
+      id: 1,
+      title: 'Test Title',
+      message: 'This is a test comment message.',
+      post: {
+        id: 1,
+        title: 'Test Post',
+        content: 'This is a test post content',
+        user: {
+          id: 1,
+          username: 'Test User',
+          email: 'test@example.com',
+          password: 'password',
+          artist: {
+            id: 1,
+            name: 'Test Artist',
+            description: 'This is a test artist description.',
+            image: 'test-artist.jpg'
+          },
+        },
+        likes: [],
+        dislikes: [],
+        image: ''
+      },
+      user: {
+        id: 1,
+        username: 'Test User',
+        email: 'test@example.com',
+        password: 'password',
+        artist: {
+          id: 1,
+          name: 'Test Artist',
+          description: 'This is a test artist description.',
+          image: 'test-artist.jpg'
+        },
+      },
+    };
+    service.createComment(mockComment).subscribe({
+      next: () => fail('Expected to fail due to invalid HTTP options'),
+      error: (error) => expect(error).toBeTruthy()
+    });
+
+    const req = httpMock.expectOne(`${environment.url}/comments`);
+    expect(req.request.method).toBe('POST');
+    req.error(new ErrorEvent('Unauthorized'), { status: 401 });
+  });
+
+  it('updateComment should update and return the comment', () => {
+    const mockComment: Comment = {
+      id: 1,
+      title: 'Test Title',
+      message: 'This is a test comment message.',
+      post: {
+        id: 1,
+        title: 'Test Post',
+        content: 'This is a test post content',
+        user: {
+          id: 1,
+          username: 'Test User',
+          email: 'test@example.com',
+          password: 'password',
+          artist: {
+            id: 1,
+            name: 'Test Artist',
+            description: 'This is a test artist description.',
+            image: 'test-artist.jpg'
+          },
+        },
+        likes: [],
+        dislikes: [],
+        image: ''
+      },
+      user: {
+        id: 1,
+        username: 'Test User',
+        email: 'test@example.com',
+        password: 'password',
+        artist: {
+          id: 1,
+          name: 'Test Artist',
+          description: 'This is a test artist description.',
+          image: 'test-artist.jpg'
+        },
+      },
+    };
+    service.updateComment(mockComment).subscribe(comment => {
+      expect(comment).toEqual(mockComment);
+    });
+
+    const req = httpMock.expectOne(`${environment.url}/comments`);
+    expect(req.request.method).toBe('PUT');
+    req.flush(mockComment);
+  });
+
+  it('updateComment should throw an error with invalid http Options', () => {
+    const mockComment: Comment = {
+      id: 1,
+      title: 'Test Title',
+      message: 'This is a test comment message.',
+      post: {
+        id: 1,
+        title: 'Test Post',
+        content: 'This is a test post content',
+        user: {
+          id: 1,
+          username: 'Test User',
+          email: 'test@example.com',
+          password: 'password',
+          artist: {
+            id: 1,
+            name: 'Test Artist',
+            description: 'This is a test artist description.',
+            image: 'test-artist.jpg'
+          },
+        },
+        likes: [],
+        dislikes: [],
+        image: ''
+      },
+      user: {
+        id: 1,
+        username: 'Test User',
+        email: 'test@example.com',
+        password: 'password',
+        artist: {
+          id: 1,
+          name: 'Test Artist',
+          description: 'This is a test artist description.',
+          image: 'test-artist.jpg'
+        },
+      },
+    };
+    service.updateComment(mockComment).subscribe({
+      next: () => fail('Expected to fail due to invalid HTTP options'),
+      error: (error) => expect(error).toBeTruthy()
+    });
+
+    const req = httpMock.expectOne(`${environment.url}/comments`);
+    expect(req.request.method).toBe('PUT');
+    req.error(new ErrorEvent('Unauthorized'), { status: 401 });
+  });
+
+  it('deleteComment should delete the comment', () => {
+    const mockComment: Comment = {
+      id: 1,
+      title: 'Test Title',
+      message: 'This is a test comment message.',
+      post: {
+        id: 1,
+        title: 'Test Post',
+        content: 'This is a test post content',
+        user: {
+          id: 1,
+          username: 'Test User',
+          email: 'test@example.com',
+          password: 'password',
+          artist: {
+            id: 1,
+            name: 'Test Artist',
+            description: 'This is a test artist description.',
+            image: 'test-artist.jpg'
+          },
+        },
+        likes: [],
+        dislikes: [],
+        image: ''
+      },
+      user: {
+        id: 1,
+        username: 'Test User',
+        email: 'test@example.com',
+        password: 'password',
+        artist: {
+          id: 1,
+          name: 'Test Artist',
+          description: 'This is a test artist description.',
+          image: 'test-artist.jpg'
+        },
+      },
+    };
+    service.deleteComment(mockComment.id!).subscribe(response => {
+      expect(response).toEqual(mockComment);
+    });
+
+    const req = httpMock.expectOne(`${environment.url}/comments/${mockComment.id}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(mockComment);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('deleteComment should throw an error with invalid http Options', () => {
+    const mockComment: Comment = {
+      id: 1,
+      title: 'Test Title',
+      message: 'This is a test comment message.',
+      post: {
+        id: 1,
+        title: 'Test Post',
+        content: 'This is a test post content',
+        user: {
+          id: 1,
+          username: 'Test User',
+          email: 'test@example.com',
+          password: 'password',
+          artist: {
+            id: 1,
+            name: 'Test Artist',
+            description: 'This is a test artist description.',
+            image: 'test-artist.jpg'
+          },
+        },
+        likes: [],
+        dislikes: [],
+        image: ''
+      },
+      user: {
+        id: 1,
+        username: 'Test User',
+        email: 'test@example.com',
+        password: 'password',
+        artist: {
+          id: 1,
+          name: 'Test Artist',
+          description: 'This is a test artist description.',
+          image: 'test-artist.jpg'
+        },
+      },
+    };
+    service.deleteComment(mockComment.id!).subscribe({
+      next: () => fail('Expected to fail due to invalid HTTP options'),
+      error: (error) => expect(error).toBeTruthy()
+    });
+
+    const req = httpMock.expectOne(`${environment.url}/comments/${mockComment.id}`);
+    expect(req.request.method).toBe('DELETE');
+    req.error(new ErrorEvent('Unauthorized'), { status: 401 });
+  });
+
+  it('getCommentsOfPost should return the comment of the post', () => {
+    const mockPostId = 1;
+    const mockComments: Comment[] = [
+      {
+        id: 1,
+        title: 'Test Title',
+        message: 'This is a test comment message.',
+        post: {
+          id: 1,
+          title: 'Test Post',
+          content: 'This is a test post content',
+          user: {
+            id: 1,
+            username: 'Test User',
+            email: 'test@example.com',
+            password: 'password',
+            artist: {
+              id: 1,
+              name: 'Test Artist',
+              description: 'This is a test artist description.',
+              image: 'test-artist.jpg'
+            },
+          },
+          likes: [],
+          dislikes: [],
+          image: ''
+        },
+        user: {
+          id: 1,
+          username: 'Test User',
+          email: 'test@example.com',
+          password: 'password',
+          artist: {
+            id: 1,
+            name: 'Test Artist',
+            description: 'This is a test artist description.',
+            image: 'test-artist.jpg'
+          },
+        },
+      },
+      {
+        id: 2,
+        title: 'Second Test Title',
+        message: 'This is a second test comment message.',
+        post: {
+          id: 1,
+          title: 'Test Post',
+          content: 'This is a test post content',
+          user: {
+            id: 1,
+            username: 'Test User',
+            email: 'test@example.com',
+            password: 'password',
+            artist: {
+              id: 1,
+              name: 'Test Artist',
+              description: 'This is a test artist description.',
+              image: 'test-artist.jpg'
+            },
+          },
+          likes: [],
+          dislikes: [],
+          image: ''
+        },
+        user: {
+          id: 2,
+          username: 'Second Test User',
+          email: 'secondtest@example.com',
+          password: 'password2',
+          artist: {
+            id: 2,
+            name: 'Second Test Artist',
+            description: 'This is a second test artist description.',
+            image: 'second-test-artist.jpg'
+          },
+        },
+      }
+    ];
+    service.getCommentsOfPost(mockPostId).subscribe(comment => {
+      expect(comment).toEqual(mockComments);
+    });
+
+    const req = httpMock.expectOne(`${environment.url}/comments/commentsByPostId/${mockPostId}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockComments);
+  });
+
+  it('getCommentsOfPost should throw an error with invalid http Options', () => {
+    const mockPostId = 1;
+    const mockComments: Comment[] = [
+      {
+        id: 1,
+        title: 'Test Title',
+        message: 'This is a test comment message.',
+        post: {
+          id: 1,
+          title: 'Test Post',
+          content: 'This is a test post content',
+          user: {
+            id: 1,
+            username: 'Test User',
+            email: 'test@example.com',
+            password: 'password',
+            artist: {
+              id: 1,
+              name: 'Test Artist',
+              description: 'This is a test artist description.',
+              image: 'test-artist.jpg'
+            },
+          },
+          likes: [],
+          dislikes: [],
+          image: ''
+        },
+        user: {
+          id: 1,
+          username: 'Test User',
+          email: 'test@example.com',
+          password: 'password',
+          artist: {
+            id: 1,
+            name: 'Test Artist',
+            description: 'This is a test artist description.',
+            image: 'test-artist.jpg'
+          },
+        },
+      },
+      {
+        id: 2,
+        title: 'Second Test Title',
+        message: 'This is a second test comment message.',
+        post: {
+          id: 1,
+          title: 'Test Post',
+          content: 'This is a test post content',
+          user: {
+            id: 1,
+            username: 'Test User',
+            email: 'test@example.com',
+            password: 'password',
+            artist: {
+              id: 1,
+              name: 'Test Artist',
+              description: 'This is a test artist description.',
+              image: 'test-artist.jpg'
+            },
+          },
+          likes: [],
+          dislikes: [],
+          image: ''
+        },
+        user: {
+          id: 2,
+          username: 'Second Test User',
+          email: 'secondtest@example.com',
+          password: 'password2',
+          artist: {
+            id: 2,
+            name: 'Second Test Artist',
+            description: 'This is a second test artist description.',
+            image: 'second-test-artist.jpg'
+          },
+        },
+      }
+    ];
+    service.getCommentsOfPost(mockPostId).subscribe({
+      next: () => fail('Expected to fail due to invalid HTTP options'),
+      error: (error) => expect(error).toBeTruthy()
+    });
+
+    const req = httpMock.expectOne(`${environment.url}/comments/commentsByPostId/${mockPostId}`);
+    expect(req.request.method).toBe('GET');
+    req.error(new ErrorEvent('Unauthorized'), { status: 401 });
+  });
+
+  //TODO Nevios Comment Tree
 });
--- client/src/app/services/GenreService/genre.service.spec.ts
+++ client/src/app/services/GenreService/genre.service.spec.ts
@@ -1,16 +1,61 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { GenreService } from './genre.service';
+import { environment } from '../../../environments/environments';
+import { Genre } from '../../models/Genre';
+import {HttpService} from "../HttpService/http.service";
 
-describe('GenreServiceService', () => {
+describe('GenreService', () => {
   let service: GenreService;
+  let httpMock: HttpTestingController;
+  let httpServiceMock: Partial<HttpService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    httpServiceMock = {
+      getHttpOptions: () => {
+        return {
+          headers: {
+            Authorization: 'Bearer null'
+          }
+        }
+      }
+    };
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GenreService, { provide: HttpService, useValue: httpServiceMock }]
+    });
     service = TestBed.inject(GenreService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('getGenres should return an Genres', () => {
+    const mockGenres: Genre[] = [
+      { id: 1, name: 'Genre One', description: 'Description One' },
+      { id: 2, name: 'Genre Two', description: 'Description Two' }
+    ];
+
+    service.getGenres().subscribe(genres => {
+      expect(genres.length).toBe(2);
+      expect(genres).toEqual(mockGenres);
+    });
+
+    const req = httpMock.expectOne(`${environment.url}/genres/all`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockGenres);
+  });
+
+  it('getGenres should throw an error with invalid http Options', () => {
+    service.getGenres().subscribe({
+      next: () => fail('Expected to fail due to invalid HTTP options'),
+      error: (error) => expect(error).toBeTruthy()
+    });
+
+    const req = httpMock.expectOne(`${environment.url}/genres/all`);
+    expect(req.request.method).toBe('GET');
+    req.error(new ErrorEvent('Unauthorized'), { status: 401 });
   });
 });
